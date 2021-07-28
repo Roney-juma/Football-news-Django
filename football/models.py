61876d959576ef5ab47f1b6b26df4633cbe9ea54
Roney-juma <ochirone10@gmail.com>
2021-07-29 00:54:55 +0300

    Heroku deployment

--- football/models.py
+++ football/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 
-# Create your models here.
-# class Category(models.Model):
+class Category(models.Model):
 
-# 	#str Helper method to return the title if we list out
-# 	def __str__(self):
-# 		return self.title
+	#str Helper method to return the title if we list out
+	def __str__(self):
+		return self.title
 
-# 	title = models.TextField(max_length=200)
+	title = models.TextField(max_length=200)
 
 
-# class Article(models.Model):
-# 	title = models.TextField(max_length=200)
-# 	author = models.TextField('Author', max_length=200)
-# 	publishedAt = models.DateTimeField('Date Published')
-# 	category = models.ForeignKey(
-#     'Category',
-#     on_delete=models.CASCADE,
-# )	#foreign key
-# 	description = models.TextField()
-# 	hero_image = models.TextField('Hero Image')
-# 	additional_image = models.TextField('Additonal Image', null=True, blank=True)
+class Article(models.Model):
+	title = models.TextField(max_length=200)
+	author = models.TextField('Author', max_length=200)
+	publishedAt = models.DateTimeField('Date Published')
+	category = models.ForeignKey(
+    'Category',
+    on_delete=models.CASCADE,
+)	#foreign key
+	description = models.TextField()
+	hero_image = models.TextField('Hero Image')
+	additional_image = models.TextField('Additonal Image', null=True, blank=True)
 
-# 	#str Helper method to return the title if we list out
-# 	def __str__(self):
-# 		return self.title
+	#str Helper method to return the title if we list out
+	def __str__(self):
+		return self.title
