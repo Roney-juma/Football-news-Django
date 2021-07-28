2294f141228a8240d1b1ee7e4c1f0207b63635bd
Roney-juma <ochirone10@gmail.com>
2021-07-28 23:01:12 +0300

    Updated the api key

--- football/views.py
+++ football/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    import requests
+    import json
+    api_request = requests.get("https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=384e999ffb31459da519c72d9c8f6ba2")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api' : api})
